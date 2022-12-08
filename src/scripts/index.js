f42fb908303ae399efda16b3717458e6e10b51b2
dollyranu <87769713+dollyranu@users.noreply.github.com>
2022-12-08 15:55:46 +0700

    First commit

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import main from '../view/main';
+
+
+document.addEventListener('DOMContentLoaded', main);
---
+++ src/view/main.js
@@ -0,0 +1,22 @@
+import restaurant from "../DATA.json";
+
+const main = () => {
+    const renderRestaurant = () => {
+        restaurant.restaurants.forEach ((restaurant) => {
+            const boxRestaurant = document.querySelector(".postCard");
+            boxRestaurant.innerHTML += `
+            <article class="item">
+              <img class="konten-gambar" src="${restaurant.pictureId}" width="300px" height="300px">
+              <div class="konten-info">
+                <p class="konten-kota">${restaurant.city}</p>
+                <h1 class="konten-nama">${restaurant.name}</h1>
+                <h3 class="konten-rating">Rating: ${restaurant.rating}</h3>
+                <p class="konten-deskripsi">${restaurant.description}</p>
+              </div>
+            </article>
+            `;
+        });
+    };
+renderRestaurant();
+};
+export default main;
