67865f6112b21eeebb37e2dfebc5ab9f94f2e5cb
Petani Kode <muhammadrazin14@gmail.com>
2022-12-08 20:40:29 +0700

    updatean bang

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,22 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 import main from '../view/main';
 
+const menu = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
+const mainElement = document.querySelector("main");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", function (event) {
+  drawer.classList.toggle("open");
+  event.stopPropagation();
+});
+
+hero.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
+
+mainElement.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
 
 document.addEventListener('DOMContentLoaded', main);
